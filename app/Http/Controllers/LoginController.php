8ad0ee957a529a5c950382ce25955a16790139d7
salmawahyuni <salma3mo@gmail.com>
2021-06-29 03:34:29 +0800

    buat register

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Auth;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -21,5 +23,21 @@ class LoginController extends Controller
     public function logout(){
         Auth::logout();
         return redirect('/');
+    }
+
+    public function registrasi(){
+        return view('Login.registrasi');
+    }
+
+    public function simpanregistrasi(Request $request){
+        User::create([
+            'name' => $request->name,
+            'level' => 'mahasiswa',
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'remember_token' => Str::random(60),
+        ]);
+
+        return view('welcome');
     }
 }
--- resources/views/Template/left-sidebar.blade.php
+++ resources/views/Template/left-sidebar.blade.php
@@ -11,7 +11,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="info">
-          <a href="#" class="d-block">PENGGUNA</a>
+          <a href="#" class="d-block">{{auth()->user()->name}}</a>
         </div>
       </div>
 
